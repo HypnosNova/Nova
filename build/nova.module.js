6c74fdadf78e2a5f6b9e2168407a43862332bc76
HypnosNova <601670314@qq.com>
2018-08-27 21:34:39 +0800

    add

--- build/nova.module.js
+++ build/nova.module.js
@@ -303,7 +303,7 @@ class World {
 const APP_STOP = 0;
 const APP_RUNNING = 1;
 const APP_PAUSE = 2;
-const VERSION = '0.1.2';
+const VERSION = '0.1.3';
 
 console.log( "Nova framework for Three.js, version: %c " + VERSION, "color:blue" );
 
